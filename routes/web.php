cae826409f165279b83a485d4803742d274cf536
jose <kcorjose78@gmail.com>
2020-04-03 01:23:11 -0400

    se agrego dos formas de agregar seguridad con middleware('auth') lo que hace es verificar si es el usuario que inicio sesion si no lo redirige a login

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,24 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+
+
+
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Route::resource('users', 'UserController')->middleware('auth');
+
+/*
+Así no habría que poner ->middelware('auth) al final de cada ruta y queda el código más limpio.
+Route::middleware(['auth'])->group(function () {
+    Route::view('/', 'welcome');
+    Route::resource('users', 'UserController');
+}
+
+*/
+
